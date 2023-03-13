dfc979b0894b4d4d28caa82ad65d11a5cf3882cc
Jesse Shawl <jesse@jesse.sh>
2023-03-12 19:25:52 -0500

    add websockets

--- src/pages/api/emit.js
+++ src/pages/api/emit.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  const io = res.socket.server.io
+  io.emit('from /api/emit', {ok: 1})
   res.status(200).json({ name: 'John Doe' })
 }
